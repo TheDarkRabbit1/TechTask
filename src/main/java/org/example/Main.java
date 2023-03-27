4eef8b2378bd2e68c20a45e721e2e5362c37e063
TDR <darkrabitthe@gmail.com>
2023-03-27 13:01:49 +0300

    menu fixes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,14 +12,14 @@ public class Main {
     private final static EquationService equationService = new EquationService();
     private final static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("PROGRAM STARTED" + "\n Menu:");
+        System.out.print("PROGRAM STARTED" + "\n Menu:");
         while (true) {
             MainMenu();
             switch (scanner.nextInt()) {
                 case 1 -> addEquationMenu();
                 case 2 -> addRootsForEquationMenu();
                 case 3 -> searchMenu();
-                case 4 -> getAllEquatinsMenu();
+                case 4 -> getAllEquationsMenu();
                 case 0 -> {return;}
                 default -> System.out.println("No such option in menu");
             }
@@ -27,8 +27,10 @@ public class Main {
     }
     private static void addRootsForEquationMenu(){
         System.out.println("Enter roots ():");
-        List<Float> roots = Arrays.stream(scanner.nextLine().split(" "))
-                .filter(s -> isFloat(s))
+        scanner.nextLine();
+        String equationInput=scanner.nextLine();
+        List<Float> roots = Arrays.stream(equationInput.split(" "))
+                .filter(Main::isFloat)
                 .map(Float::parseFloat)
                 .collect(Collectors.toList());
         System.out.println("Enter equation body or id:");
@@ -59,29 +61,39 @@ public class Main {
             return false;
         }
     }
-    private static void getAllEquatinsMenu(){
+    private static void getAllEquationsMenu(){
         equationService.getEqations().forEach(e -> System.out.print(e.toString()));
     }
     private static void addEquationMenu(){
         System.out.println("Input equation:");
+        scanner.nextLine();
         Equation equation = new Equation(scanner.nextLine());
         equationService.addEquation(equation);
-        System.out.println("Equation is viable:" + equation.isViable()
+        System.out.println("Equation with id "+equation.getId()+" is viable:" + equation.isViable()
                 + " contains " + equation.getNumbers() + " numbers");
     }
     private static void searchMenu(){
         System.out.println("1 - Root:" + "\n 2 - Has only one root");
         switch (scanner.nextInt()) {
             case 1 -> {
+                scanner.nextLine();
                 System.out.println("enter root:");
                 equationService.searchByRoot(scanner.nextFloat())
                         .forEach(e-> System.out.print(e.toString()));
             }
-            case 2 -> equationService.oneRootOnlyEquations().forEach(e -> System.out.print(e.toString()));
+            case 2 -> {
+                List<Equation> equations = equationService.oneRootOnlyEquations();
+                if (equations.isEmpty()){
+                    System.out.println("List of such equations is empty");
+                }else{
+                    equations.forEach(e -> System.out.print(e.toString()));
+                }
+            }
             default -> System.out.println("not an option");
         }
     }
     private static void MainMenu(){
+        System.out.println("\n");
         System.out.println("1 - Add new Equation");
         System.out.println("2 - Add roots for Equation");
         System.out.println("3 - Search Equation by:");
